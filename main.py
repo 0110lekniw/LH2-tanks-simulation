f18955dd181187817705d55289962f766d321a4c
lekniw0110 <chojnacki.adrian@yahoo.com>
2021-12-30 19:05:11 +0100

    added pressure change claculation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from define_mission import calculateChanges, defineProfile, defineAirProfile
 from input_data import data_options, importData, importConfiguration
 from fluid_properities import calculateHydrogen
-from thermal_calculations import calculateConductiveHeat, calculateHeat
+from thermal_calculations import calculateConductiveHeat, calculateHeat, calculateTankChange
 
 data = importData(data_options[0])
 configuration = importConfiguration()
@@ -15,12 +15,14 @@ changes = calculateChanges(data["mission_profile"], False, dt=10)
 time_steps = changes[:, 0]
 profile = defineProfile(changes, configuration["reserve deviation"], configuration["reserve rate"], evaporation=0)
 airProfile = defineAirProfile(profile)
+# ["P", "D", "T", "H", "volume", "mass", density_star, mass lh2, mass gh2, energy derivative]
 hydrogenData = calculateHydrogen(pressure=configuration["pressure_tank"], volume_tot=tank[2],
                                  fill_level=configuration["lh2_fill_0"])
 conductive_heat = calculateConductiveHeat(area=tank[1], conductivity_coefficient=insulation[0],
                                           thickness=insulation_thickness, hot_surface_temperature=airProfile[0, 0],
                                           cold_surface_temperature=hydrogenData[2])
 heat = calculateHeat(conductive_heat=conductive_heat, time_step=time_steps[1])
+tank_change = calculateTankChange(heat=heat, energy_derivative=hydrogenData[-1], total_volume=hydrogenData[4], )
 
 
 print(heat)
--- thermal_calculations.py
+++ thermal_calculations.py
@@ -1,8 +1,33 @@
-def calculateConductiveHeat(area, conductivity_coefficient, thickness, hot_surface_temperature, cold_surface_temperature):
-    heat = conductivity_coefficient*10**(-3)*area*abs(hot_surface_temperature -cold_surface_temperature)/thickness
+from sympy import nsolve, Symbol
+
+
+def calculateConductiveHeat(area, conductivity_coefficient, thickness, hot_surface_temperature,
+                            cold_surface_temperature):
+    heat = conductivity_coefficient * 10 ** (-3) * area * abs(
+        hot_surface_temperature - cold_surface_temperature) / thickness
     return heat
 
 
-def calculateHeat(conductive_heat = 0, radiation_heat = 0, convection_heat = 0, time_step = 0):
-    heat = 1.3*(conductive_heat + radiation_heat + convection_heat)*time_step
+def calculateHeat(conductive_heat=0, radiation_heat=0, convection_heat=0, time_step=0):
+    heat = 1.3 * (conductive_heat + radiation_heat + convection_heat) * time_step
     return heat
+
+
+def calculateTankChange(heat=0, energy_derivative=0, total_volume=0, total_engine_flow=0, vaporization_heat=0,
+                            density_star=0, vented_pressure=0, tank_pressure=101325):
+    if tank_pressure == vented_pressure:
+        x = Symbol('x')
+        pressure_change = 0
+        vented_flow = nsolve(
+            heat - (total_engine_flow + x) * vaporization_heat * (x / (total_engine_flow + x) + density_star), x, 0)
+    else:
+        pressure_change = energy_derivative / total_volume * (
+                    heat - total_engine_flow * vaporization_heat * density_star)
+        if tank_pressure + pressure_change < vented_pressure:
+            vented_flow = 0
+        else:
+            x = Symbol('x')
+            vented_flow = nsolve(energy_derivative / total_volume *
+                                 (heat - (total_engine_flow + x) * vaporization_heat *
+                                  (x / (total_engine_flow + x) + density_star)) - pressure_change, x, 0)
+    return [pressure_change, vented_flow]
